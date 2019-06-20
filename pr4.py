c2dbf105131fd168b9df5bf6b720df46dbf12e18
anbumagan <50615722+anbumagan@users.noreply.github.com>
2019-06-20 21:38:57 +0530

    Update pr4.py

--- pr4.py
+++ pr4.py
@@ -2,10 +2,10 @@ a1=int(input())
 l=list(map(int,input().split()))
 n=sum(l)
 c=0
-a=a1//l[0]
-for i in range(0,a-2):
-    for j in range(i+1,a-1):
+a=len(l)
+for i in range(0,a):
+    for j in range(i+1,a):
         for k in range(j+1,a):
-            if(l[i]*l[j]*l[k]==n):
+            if(l[i]<l[j]<l[k] and i<j<k):
                 c=c+1
 print(c)
