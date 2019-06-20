7777cc4fa64688effe0d0ec8ad00b6f461d6eca5
anbumagan <50615722+anbumagan@users.noreply.github.com>
2019-06-20 20:24:40 +0530

    Update pr3.py

--- pr3.py
+++ pr3.py
@@ -1,22 +1,9 @@
-a,b=map(str,input().split())
-c=len(a)
-d=len(b)
-if(c==b):
-    for i in range(c):
-        if(a[i]!=b[i]):
-            c=c+1
-    print(c)
-elif(c<d):
-    d1=a.ljust(d,'0')
-    c=0
-    for i in range(d):
-        if(d1[i]!=b[i]):
-            c=c+1
-    print(c)
-else:
-    d2=b.ljust(c,'0')
-    c=0
-    for i in range(c):
-        if(d2[i]!=a[i]):
-            c=c+1
-    print(c)
+#simple_cost
+a,b=input().split()
+t=abs(len(a)-len(b))
+for i in range(len(a)):
+    if len(b)==1 and b[i] in a:
+        break
+    if a[i]!=b[i]:
+        t+=1
+print(t)
