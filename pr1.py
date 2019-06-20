7fe9dc30742533b1e097fd4c66909b2046423b37
anbumagan <50615722+anbumagan@users.noreply.github.com>
2019-06-20 17:39:44 +0530

    Create pr1.py

---
+++ pr1.py
@@ -0,0 +1,22 @@
+a=int(input())
+l=[]
+for i in range(0,a):
+    c=str(input())
+    l.append(c)
+def compare(st1,st2):
+    a1=len(st1)
+    a2=len(st2)
+    res=""
+    i=0
+    j=0
+    while(i<=a1-1 and j<=a2-1):
+        if(st1[i]==st2[j]):
+            res=res+st1[i]
+        i=i+1
+        j=j+1
+    return res
+op=[]
+for i in range(len(l)-1):
+    op.append(compare(l[i],l[i+1]))
+    i=i+1
+print(set(op))
