c0319164026e7b72373bf94ddc74ea1b6f9ac8e6
anbumagan <50615722+anbumagan@users.noreply.github.com>
2019-06-21 02:47:24 +0530

    Update pr33.py

--- pr33.py
+++ pr33.py
@@ -1,6 +1,20 @@
 f=str(input())
-a=len(f)
+a=len(f[::-1])
 l=[]
+v=[]
+w=[]
 for i in range(0,a):
-    l.append(f[i+1:])
-print(sorted(l)[len(l)-1])
+    l.append([f[i+1:],len(f[i+1:])])
+    v.append([len(f[i+1:]),f[i+1:]])
+l.sort()
+v.sort()
+l=max(l)
+v=max(v)
+w.append(l[0])
+w.append(v[1])
+w.append(f[0])
+l=[]
+for i in w:
+    if(i[0]>f[0]):
+        l.append(i)
+print(sorted(l)[0])
