aa033b6a208511a9d60543d4e8194d4c61e24fab
anbumagan <50615722+anbumagan@users.noreply.github.com>
2019-06-28 12:47:11 +0530

    Update pr63.py

--- pr63.py
+++ pr63.py
@@ -1,18 +1,3 @@
-'''a=str(input())
-b=len(a)
-c=[]
-for i in range(0,b):
-    for j in range(i+1,b):
-        c.append(a[i:j+1])
-d=[]
-for i in c:
-    if(len(i)==len(set(i))):
-        d.append(i)
-d.sort(key=len)
-d=d[::-1]
-print(len(d[0]))'''
-
-
 q=input()
 l1=[]
 l=len(q)
